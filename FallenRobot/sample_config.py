37e129a1fca336491e2ac956e625f3c57056c882
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2022-06-27 19:14:54 +0530

    Update sample_config.py

--- FallenRobot/sample_config.py
+++ FallenRobot/sample_config.py
@@ -17,14 +17,14 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 1356469075  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "anonymous_was_bot"
-    SUPPORT_CHAT = "DevilsHeavenMF"  # Your own group for support, do not add the @
+    OWNER_ID = 1484735126  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "AerodynamicV1_OFFICIAL"
+    SUPPORT_CHAT = "AerodynamicV1_Promotion"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001686672798
+        -1001587456410
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001686672798
+        -1001587456410
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
