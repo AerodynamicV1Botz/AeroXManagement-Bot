d2f9466b2da3dbe1a34ff7e4b2df2fe5fb6bcb00
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2022-06-27 18:09:12 +0530

    Update google.py

--- FallenRobot/modules/google.py
+++ FallenRobot/modules/google.py
@@ -283,6 +283,6 @@ __help__ = """
  ❍ /img <text>*:* Search Google for images and returns them\nFor greater no. of results specify lim, For eg: `/img hello lim=10`
  ❍ /app <appname>*:* Searches for an app in Play Store and returns its details.
  ❍ /reverse: Does a reverse image search of the media which it was replied to.
- ❍ Anonymous <query>*:* Fallen answers the query
+ ❍ Anonymous <query>*:* AeroXManagement-Bot answers the query
   💡Ex: `Anonymous where is India?`
 """
