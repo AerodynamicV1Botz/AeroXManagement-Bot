2e85610244250cf8dd52b439bfc29ed8b51a1d5c
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2022-07-22 11:56:06 +0530

    Update writetool.py
    Signed-off-by: AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>

--- FallenRobot/modules/writetool.py
+++ FallenRobot/modules/writetool.py
@@ -1,27 +1,75 @@
+import requests
+from requests import get
 from pyrogram import filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
-from FallenRobot import pbot
-
-
-@pbot.on_message(filters.command("write"))
-async def handwriting(_, message):
-    if len(message.command) < 2:
-        return await message.reply_text("» ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ ᴡʀɪᴛᴇ ɪᴛ ᴏɴ ᴍʏ ᴄᴏᴩʏ...")
-    m = await message.reply_text("» ᴡᴀɪᴛ ᴀ sᴇᴄ, ʟᴇᴛ ᴍᴇ ᴡʀɪᴛᴇ ᴛʜᴀᴛ ᴛᴇxᴛ...")
-    name = (
-        message.text.split(None, 1)[1]
-        if len(message.command) < 3
-        else message.text.split(None, 1)[1].replace(" ", "%20")
-    )
-    hand = "https://apis.xditya.me/write?text=" + name
-    await m.edit("» ᴜᴩʟᴏᴀᴅɪɴɢ...")
-    await pbot.send_chat_action(message.chat.id, "upload_photo")
-    await message.reply_photo(
-        hand, caption="ᴡʀɪᴛᴛᴇɴ ᴡɪᴛʜ 🖊 ʙʏ [ғᴀʟʟᴇɴ](t.me/FallenXRobot)"
-    )
+from FallenRobot import pbot as fallen, dispatcher, SUPPORT_CHAT
 
 
-__mod_name__ = "Hᴀɴᴅᴡʀɪᴛᴇ"
+@fallen.on_message(filters.command("write"))
+async def handwrite(_, message: Message):
+    if not message.reply_to_message:
+        name = (
+            message.text.split(None, 1)[1]
+            if len(message.command) < 3
+            else message.text.split(None, 1)[1].replace(" ", "%20")
+        )
+        m = await fallen.send_message(
+            message.chat.id, "**ᴩʟᴇᴀsᴇ ᴡᴀɪᴛ...**\n\nʟᴇᴍᴍᴇ ᴡʀɪᴛᴇ ɪᴛ ᴏɴ ᴍʏ ᴄᴏᴩʏ..."
+        )
+        photo = "https://apis.xditya.me/write?text=" + name
+        caption = f"""
+sᴜᴄᴄᴇssғᴜʟʟʏ ᴡʀɪᴛᴛᴇɴ ᴛᴇxᴛ 💘
+
+✨ **ᴡʀɪᴛᴛᴇɴ ʙʏ :** [{dispatcher.bot.first_name}](https://t.me/{dispatcher.bot.username})
+🥀 **ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ :** {message.from_user.mention}
+"""
+        await fallen.send_photo(
+            message.chat.id,
+            photo=photo,
+            caption=caption,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            "• sᴜᴩᴩᴏʀᴛ •", url=f"https://t.me/{SUPPORT_CHAT}"
+                        )
+                    ]
+                ]
+            ),
+        )
+        await m.delete()
+    else:
+        lol = message.reply_to_message.text
+        name = lol.split(None, 0)[0].replace(" ", "%20")
+        m = await fallen.send_message(
+            message.chat.id, "**ᴩʟᴇᴀsᴇ ᴡᴀɪᴛ...**\n\nʟᴇᴍᴍᴇ ᴡʀɪᴛᴇ ɪᴛ ᴏɴ ᴍʏ ᴄᴏᴩʏ..."
+        )
+        photo = "https://apis.xditya.me/write?text=" + name
+        caption = f"""
+sᴜᴄᴄᴇssғᴜʟʟʏ ᴡʀɪᴛᴛᴇɴ ᴛᴇxᴛ 💘
+
+✨ **ᴡʀɪᴛᴛᴇɴ ʙʏ :** [{dispatcher.bot.first_name}](https://t.me/{dispatcher.bot.username})
+🥀 **ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ :** {message.from_user.mention}
+"""
+        await fallen.send_photo(
+            message.chat.id,
+            photo=photo,
+            caption=caption,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            "• sᴜᴩᴩᴏʀᴛ •", url=f"https://t.me/{SUPPORT_CHAT}"
+                        )
+                    ]
+                ]
+            ),
+        )
+        await m.delete()
+
+
+__mod_name__ = "WʀɪᴛᴇTᴏᴏʟ"
 
 __help__ = """
 
